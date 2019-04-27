f9fe21877684fae37132e7fc09b436898d0af267
Prouser123 <jcahill773@gmail.com>
2019-04-27 17:31:26 +0100

    clean up bd reload pt.1

--- src/java/me/prouser123/bungee/discord/commands/ingame/sub/Reload.java
+++ src/java/me/prouser123/bungee/discord/commands/ingame/sub/Reload.java
@@ -14,35 +14,32 @@ public class Reload {
 	public static void execute(CommandSender sender) {
     	Main.getMCM().load();
     	sender.sendMessage(new TextComponent(InGameCommand.prefix + ChatColor.DARK_GREEN + "Reloaded from config."));
-    	// Log all variables for debugging
-    	sender.sendMessage(new TextComponent("token: " + Main.getMCM().getToken()));
-    	sender.sendMessage(new TextComponent("jclid: " + Main.getMCM().getJoinLeaveChatId()));
-    	sender.sendMessage(new TextComponent("debug: " + Main.getMCM().getDebugEnabled()));
-    	sender.sendMessage(new TextComponent("status: " + Main.getMCM().getStatus()));
     	
     	// Reconnect DiscordApi if it is null
     	if (Discord.api == null) {
     		new Discord(Main.getMCM().getToken());
     		if (Discord.api != null) {
                 Main.inst().setLocalBotOptions();
-                Main.inst().getLogger().info("bd reload: token " + Main.getMCM().getToken());
                 sender.sendMessage(new TextComponent(InGameCommand.connectedAsUser(true)));
     		}
     	}
-    	
-    	// Update status
-    	Discord.setStatus();
-    	
+
     	// reload the debug logger
     	Main.debugLogger = new DebugLogger();
-    	
-    	// reload player join leave
-		Main.registerListeners.playerJoinLeave();
-		
-		if (Main.registerListeners.jlcidEnabled) {
-        	sender.sendMessage(new TextComponent(InGameCommand.jlcEnabledForChannel()));
-		} else {
-			sender.sendMessage(new TextComponent(InGameCommand.prefix + ChatColor.RED + "Join Leave Chat disabled. Did you put a valid channel id in the config?"));
-		}
+
+    	if (Discord.api != null) {
+    		// Update status
+    		Discord.setStatus();
+    		
+    		// reload player join leave
+    		Main.registerListeners.playerJoinLeave();
+
+    		// Message about join leave status
+    		if (Main.registerListeners.jlcidEnabled) {
+            	sender.sendMessage(new TextComponent(InGameCommand.jlcEnabledForChannel()));
+    		} else {
+    			sender.sendMessage(new TextComponent(InGameCommand.prefix + ChatColor.RED + "Join Leave Chat disabled. Did you put a valid channel ID in the config?"));
+    		}
+    	}
 	}
 }
