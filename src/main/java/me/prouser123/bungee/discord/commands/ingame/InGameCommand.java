6811a8fcded373a9fc18062934f433c32868802e
James Cahill <jcahill773@gmail.com>
2019-09-02 12:02:47 +0100

    Remove debug /bd save command

--- src/main/java/me/prouser123/bungee/discord/commands/ingame/InGameCommand.java
+++ src/main/java/me/prouser123/bungee/discord/commands/ingame/InGameCommand.java
@@ -30,36 +30,18 @@ public class InGameCommand extends Command {
 		// bd command
 		if ( args.length == 0 ) {
 			me.prouser123.bungee.discord.commands.ingame.sub.Main.execute(sender);
-            
 			
         // bd help command
         } else if (args[0].equalsIgnoreCase("help")) {
         	Help.execute(sender);
-
             
-        // bd save command (testing purposes only)
-        } else if (args[0].equalsIgnoreCase("save")) {
-        	if (args.length == 1) {
-            	Main.getMCM().write();
-				sender.sendMessage(new TextComponent(prefix + ChatColor.DARK_GREEN + "Saved!"));
-        	} else if (args[1].equalsIgnoreCase("token")) {
-        		Main.getMCM().setToken(args[2]);
-        	} else if (args[1].equalsIgnoreCase("jlcid")) {
-        		Main.getMCM().setJoinLeaveChatId(args[2]);
-        	} else if (args[1].equalsIgnoreCase("debug")) {
-        		Main.getMCM().setDebugEnabled(Boolean.parseBoolean(args[2]));
-        	}
-        	
-        	
         // bd reload command
         } else if (args[0].equalsIgnoreCase("reload")) {
         	Reload.execute(sender);
         	
-        	
         // bd invite command
         } else if (args[0].equalsIgnoreCase("invite")) {
         	Invite.execute(sender);
-        	
         	
         // bd token command
         } else if (args[0].equalsIgnoreCase("token")) {
@@ -72,7 +54,6 @@ public class InGameCommand extends Command {
         // bd debug command
         } else if (args[0].equalsIgnoreCase("debug")) {
         	Debug.execute(sender);
-        	
         	
         // Show generic usage help command
         } else {
