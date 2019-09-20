f3c73152649dd8e9c055226cfc1da65feb10fc75
James Cahill <jcahill773@gmail.com>
2019-09-20 13:21:21 +0100

    [2.0.1] Improved player messages - less spammy.

--- src/java/me/prouser123/bungee/discord/commands/Players.java
+++ src/java/me/prouser123/bungee/discord/commands/Players.java
@@ -1,16 +1,16 @@
 package me.prouser123.bungee.discord.commands;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
-
+import java.util.Map;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
 
 import me.prouser123.bungee.discord.Main;
 import me.prouser123.bungee.discord.base.BaseCommand;
-import me.prouser123.bungee.discord.base.BaseCommand.base;
+import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
 public class Players implements MessageCreateListener, BaseCommand {
@@ -30,24 +30,34 @@ public class Players implements MessageCreateListener, BaseCommand {
                     .setDescription("All players currently online on the network.")
                 	.setFooter("Bungee Discord " + Main.inst().getDescription().getVersion().toString() + " | !bd"/*.split("-")[0]*/, "https://cdn.discordapp.com/avatars/215119410103451648/575d90fdda8663b633e36f8b8c06c719.png");
         	
-        	// Create an array of players and their servers
-            List<String> players = new ArrayList<>();
-            
-            for ( ProxiedPlayer player : Main.inst().getProxy().getPlayers() )
-            {
-                players.add(player.getDisplayName() + " at " + player.getServer().getInfo().getName());
-            }
-            Collections.sort( players, String.CASE_INSENSITIVE_ORDER );
-            
-            if (players.size() == 0) {
+        	// Make sure some players are online first
+        	if (Main.inst().getProxy().getPlayers().size() == 0) {
+        		// If not, just send this message.
             	embed2.setDescription("There are no players online.");
+                event.getChannel().sendMessage(embed2);
+                return;
+            	
             }
         	
+        	// Create an array of servers and their players
+            Map<String, List<String>> map =  new HashMap<String, List<String>>();
+            
+            // Create a list for every server
+            for(ServerInfo server : Main.inst().getProxy().getServers().values()) {
+            	
+            	// Add the new list to the HashMap
+                map.put(server.getName(), new ArrayList<>());
+            }
+            
+            // Add the players to the map
+            for ( ProxiedPlayer player : Main.inst().getProxy().getPlayers() )
+            {
+                map.get(player.getServer().getInfo().getName()).add(player.getDisplayName());
+            }
+            
         	// Create a field in the embed for every player online
-            for(String player : players) {
-            	String[] playerArray = player.split(" at ");
-            	// Player name is item 0, Player server is item 1
-            	embed2.addField(playerArray[0], "online at " + playerArray[1]);
+            for(Map.Entry<String, List<String>> entry : map.entrySet()) {
+                embed2.addField(entry.getKey(), entry.getValue().toString().replace("[", "").replace("]", ""));
             }
         	
         	// Send the embed
